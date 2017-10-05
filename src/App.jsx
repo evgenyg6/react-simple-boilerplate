b6612e0ed4aba2ff3c97da75f8117e3ae25c0cf1
evgenyg6 <evgenyg@live.com>
2017-10-05 00:01:13 +0000

    Moved addEventListener into componentDidMount

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import ChatBar from './ChatBar.jsx';
 let newSocket = new WebSocket("ws:localhost:3001");
 
 class App extends Component {
-
   constructor(props) {
     super(props);
     this.messageBox = this.messageBox.bind(this);
@@ -14,24 +13,23 @@ class App extends Component {
       messages: []
     };
   }
+
+/*  handleChange(event) {
+    this.setState({currentUser: event.target.value});
+  }*/
+
+
+
   // Grabs messages from chatbarMessage and chatbarUsername and displays them
   messageBox(newUser, message) {
-
       // Update the state of the app component.
       // Add a new message to the list of messages in the data store
       let msg = {
         username: newUser,
         content: message
       };
-      // Concatinates, converts object to JSON and sends to server
-      newSocket.addEventListener('message', (event) =>{
-      const allMessages = this.state.messages.concat(JSON.parse(event.data));
-      // Calling setState will trigger a call to render() in App and all child components.
-      this.setState({messages: allMessages});
-      document.getElementById('chatbarMessage').value = '';
-      })
-      newSocket.send(JSON.stringify(msg));
 
+      newSocket.send(JSON.stringify(msg));
   }
   render() {
     return (
@@ -46,10 +44,17 @@ class App extends Component {
   }
 
   componentDidMount(){
+    // Concatinates, converts object to JSON and sends to server
+      newSocket.addEventListener('message', (event) =>{
+      const allMessages = this.state.messages.concat(JSON.parse(event.data));
+      // Calling setState will trigger a call to render() in App and all child components.
+      this.setState({messages: allMessages});
+      document.getElementById('chatbarMessage').value = '';
+      })
 
-  newSocket.onopen = function(){
-  newSocket.send(JSON.stringify({content: 'You are now connected.'}));
-  }
+    newSocket.onopen = function(){
+    newSocket.send(JSON.stringify({content: 'You are now connected.'}));
+    }
 
   }
 }
